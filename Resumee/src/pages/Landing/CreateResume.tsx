42cb4c2a82b4843c74c474bb56ad8c8b1a933685
Ifeoluwa <onabajoif@yahoo.com>
2025-03-04 15:39:35 +0100

    it's a lot

--- Resumee/src/pages/Landing/CreateResume.tsx
+++ Resumee/src/pages/Landing/CreateResume.tsx
@@ -1,20 +1,87 @@
 // import { useEffect, useState } from "react"; useFormHook
 // import { useForm } from "react-hook-form"
+import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+// import { toast } from "react-toastify";
+import axios from "axios";
 import { UserAcademyForm, UserAddressForm, UserContactForm, UserInfoForm } from "../../components/form";
 import { useResumePage, useFormHook } from "../../hooks"
 import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 import View from "../../components/resumeview/View";
 // import { IFormInput } from "../../types/types";
 export const CreateResume = () => {
+    const location = useLocation()
     const {
         resumePageInfo: { current },
         updateCurrent
     } = useResumePage()
 
     const {
-        resumeInfo
+        resumeInfo,
+        updateAddress,
+        updateCity,
+        updateCountry,
+        updateDOB,
+        updateEmail,
+        updateFax,
+        updateFirstName,
+        updateGender,
+        updateLastName,
+        updateLinkedInLink,
+        updateOccupation,
+        updatePhoneNumber,
+        updateState,
+        updateZipcode
     } = useFormHook()
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const getResumeInfo = async (id: string) => {
+            try {
+                const res = await axios.get(`http://localhost:3000/user/${id}`);            
+                if(res.status !== 400) {
+                    const userinfo = res.data
+                    updateDOB(userinfo.dob)
+                    updateGender(userinfo.gender)
+                    updateLastName(userinfo.lastName)
+                    updateFirstName(userinfo.firstName)
+                    updateOccupation(userinfo.occupation)
+                    updateAddress(userinfo.address.address)
+                    updateCity(userinfo.address.state)
+                    updateState(userinfo.address.state)
+                    updateCountry(userinfo.address.country)
+                    updateZipcode(userinfo.address.zipcode)
+                    updateEmail(userinfo.contact.email)
+                    updateFax(userinfo.contact.faxNo)
+                    updateLinkedInLink(userinfo.contact.linkedInUrl)
+                    updatePhoneNumber(userinfo.contact.phoneNumber)
+                }
+            } catch(error: any) {
+              let err = error.response
+              console.log(err)
+            //   if(Array.isArray(err.data.message)) {
+            //       err.data.message.forEach((msg: string) => {
+            //           toast(msg, {
+            //               type: "error"
+            //           })
+            //       })
+            //   } else {
+            //     toast(err.data.message, {
+            //       type: "error"
+            //     })
+            //   }
+              navigate('/')
+            }
+        }
+        if(location.pathname === "/edit-resume") {
+            const queryParams = new URLSearchParams(location.search);
+            const id = queryParams.get("id");
+            if(id) {
+                getResumeInfo(id)
+            }          
+        }
+    }, [])
 
     useEffect(() => {
         if(current === 'review page') {
--- Resumee/src/pages/Landing/Resume.tsx
+++ Resumee/src/pages/Landing/Resume.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useParams, useNavigate } from "react-router"
+import { useParams, useNavigate, NavLink } from "react-router"
 import axios from "axios"
 import { toast } from "react-toastify"
 import View from "../../components/resumeview/View"
@@ -53,25 +53,27 @@ export const Resume = () => {
     }
 
     getResumeInfo()
-  })
+  }, [])
 
   return (
-    <div className="w-[60%] h-[85vh] mx-auto overflow-scroll">
-      <div id="cta" className="mb-[3em] flex justify-end">
-        <div>
-          <button className="w-[200px] p-2 mr-4 bg-secondary-2 text-white text-xl shadow-md shadow-tertiary-2 rounded-md cursor-pointer">Edit</button>
-          <button
-             className="w-[200px] p-2 mr-4 bg-red-400 text-white text-xl shadow-md shadow-tertiary-2 rounded-md cursor-pointer"
-            onClick={() => {
-              deteleResume()
-            }}
-          >Delete</button>
-        </div>
-      </div>
-      {isLoading && <p>Fetching resource for resume with id: {id}</p>}
+    <div className="overflow-scroll">
+      {isLoading && <p className="text-2xl text-center mt-[4em]">Fetching resource for resume with id: {id}.....</p>}
       {!isLoading &&
-        <div>
-          <View result={data} />
+        <div className="w-[60%] h-[85vh] mx-auto ">
+          <div id="cta" className="mb-[3em] flex justify-end">
+            <div className="flex">
+              <NavLink to={`/edit-resume?id=${id}`} className="block w-[200px] p-2 mr-4 bg-secondary-2 text-white text-center text-xl shadow-md shadow-tertiary-2 rounded-md cursor-pointer">Edit</NavLink>
+              <button
+                className="w-[200px] p-2 mr-4 bg-red-400 text-white text-xl shadow-md shadow-tertiary-2 rounded-md cursor-pointer"
+                onClick={() => {
+                  deteleResume()
+                }}
+              >Delete</button>
+            </div>
+          </div>
+          <div>
+            <View result={data} />
+          </div>
         </div>}
     </div>
   )
